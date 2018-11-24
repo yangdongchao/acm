52c436e50922e251618461dfd792cb6bf7315009
杨东超 <15087581161@163.com>
2018-11-24 22:11:05 +0800

    Create 最大闭权子图.cpp

---
+++ 网络流模板/最大闭权子图.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+
+struct Edge
+{
+    int from, to, cap, flow;
+    Edge(int u, int v, int c, int f): from(u), to(v), cap(c), flow(f) {}
+};
+
+const int maxn = 505;
+struct Dinic
+{
+    int n, m, s, t;
+    vector<Edge> edges;
+    vector<int> G[maxn];
+    bool vis[maxn];
+    int d[maxn];
+    int cur[maxn];
+    void init(int n)
+    {
+        this->n = n;
+        for (int i = 0; i < n; i++) G[i].clear();
+        edges.clear();
+    }
+    void AddEdge(int from, int to, int cap)
+    {
+        edges.emplace_back(from, to, cap, 0);
+        edges.emplace_back(to, from, 0, 0);
+        m = edges.size();
+        G[from].push_back(m - 2);
+        G[to].push_back(m - 1);
+    }
+    bool BFS()
+    {
+        memset(vis, 0, sizeof(vis));
+        memset(d, 0, sizeof(d));
+        queue<int> q;
+        q.push(s);
+        d[s] = 0;
+        vis[s] = 1;
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            for (int i = 0; i < G[x].size(); i++)
+            {
+                Edge& e = edges[G[x][i]];
+                if (!vis[e.to] && e.cap > e.flow)
+                {
+                    vis[e.to] = 1;
+                    d[e.to] = d[x] + 1;
+                    q.push(e.to);
+                }
+            }
+        }
+        return vis[t];
+    }
+    int DFS(int x, int a)
+    {
+        if (x == t || a == 0) return a;
+        int flow = 0, f;
+        for (int& i = cur[x]; i < G[x].size(); i++)
+        {
+            Edge& e = edges[G[x][i]];
+            if (d[x] + 1 == d[e.to] && (f = DFS(e.to, min(a, e.cap - e.flow))) > 0)
+            {
+                e.flow += f;
+                edges[G[x][i] ^ 1].flow -= f;
+                flow += f;
+                a -= f;
+                if (a == 0) break;
+            }
+        }
+        return flow;
+    }
+    int Maxflow(int s, int t)
+    {
+        this->s = s, this->t = t;
+        int flow = 0;
+        while (BFS())
+        {
+            memset(cur, 0, sizeof(cur));
+            flow += DFS(s, INF);
+        }
+        return flow;
+    }
+} ans;
+int n;
+int main(int argc, char const *argv[])
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    scanf("%d",&n);
+    ans.init(n+10);
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>0) ans.AddEdge(0,i,x);
+        if(x<0) ans.AddEdge(i,n+1,-x);
+        if(x>0) sum+=x;
+        for(int j=i+i;j<=n;j+=i)
+        {
+            ans.AddEdge(i,j,INF);
+        }
+    }
+    int res=sum-ans.Maxflow(0,n+1);
+    cout<<res<<endl;
+    return 0;
+}
